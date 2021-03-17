4c22972b401d9e4a89b1d550869992c37ac8fa55
Mikael Lundin <mikael.lundin@bouvet.se>
2021-03-17 15:41:36 +0100

    second endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hellou World of Clouds!\n');
 });
 
+app.get('/another', function (req, res) {
+  res.send('Hellou anouther!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
