e3de2510823af5ec86d0be1650d1fadae59c681a
Mikael Lundin <mikael.lundin@bouvet.se>
2021-03-17 16:10:47 +0100

    try html

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,11 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hellou World of Clouds!\n');
+  res.send('<html><body bgcolor=#ffff00>Lets try some good ol HTML!</body></html>');
 });
 
 app.get('/another', function (req, res) {
-  res.send('Hellou anouther!\n');
+  res.send('<html><body bgcolor=#aaff00>Lets try another!</body></html>');
 });
 
 app.listen(8080, function () {
