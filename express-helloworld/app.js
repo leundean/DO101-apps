eed1fa05f1d67f7216b8c106b710d522a6ed6b8d
Mikael Lundin <mikael.lundin@bouvet.se>
2021-03-18 08:12:16 +0100

    ep3

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/another', function (req, res) {
   res.send('<html><body bgcolor=#aaff00>Lets try another!</body></html>');
 });
 
+app.get('/3', function (req, res) {
+  res.send('<html><body bgcolor=#00aaff>EP3</body></html>');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
